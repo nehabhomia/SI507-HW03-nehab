061cbef91b0b76aa45b86ed03f2e57bfc41b7897
Neha Bhomia <nehabhomia@hotmail.co.uk>
2018-09-19 10:30:17 -0400

    added code to determine if response question

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,6 @@
 my_responses = input("What is your question?")
+
+while my_responses != "quit":
+	while my_responses[-1] != "?":
+		print("I'm sorry, I can only answer questions.")
+		my_responses = input("What is your question?")
