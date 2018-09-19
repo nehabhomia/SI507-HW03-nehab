02d6e7b9779ff90f5de8a52959f416a4d10c0c24
Danny Teng <tengdann@umich.edu>
2018-09-19 10:51:16 -0400

    Fixed minor bugs

--- magic_eight.py
+++ magic_eight.py
@@ -6,15 +6,15 @@ poss_answers = [ "It is certain.", "It is decidely so.", "Without a doubt.", "Ye
 "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.",
 "My sources say no.", "Outlook not so good.", "Very doubtful."]
 
-my_responses = input("What is your question?")
+my_responses = input("What is your question? ")
 
 while my_responses != "quit":
-	while my_responses[-1] != "?":
-		print("I'm sorry, I can only answer questions.")
-		my_responses = input("What is your question?")
-
+    while my_responses[-1] != "?":
+        print("I'm sorry, I can only answer questions.")
+        my_responses = input("What is your question? ")
+        
     x = random.randrange(0, 20)
-
+    
     print (poss_answers[x])
     
-    my_response = input("What is your question?")
+    my_responses = input("What is your question? ")
