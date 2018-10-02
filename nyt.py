9aa9fa104540ee71d6e64b1e47a66d4d0af5ffa1
Neha Bhomia <nehabhomia@hotmail.co.uk>
2018-10-02 10:55:48 -0400

    adding nyt.py

---
+++ nyt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  2 10:43:32 2018
+
+@author: nehabhomia
+"""
+
+from secrets import *
+
+import requests
+
+# gets stories from a particular section of NY times
+def get_stories(section):
+    baseurl ='https://api.nytimes.com/svc/topstories/v2/'
+    extendedurl = baseurl + section +'.json'
+    params={'api-key': nyt_key}
+    return requests.get(extendedurl, params).json()
+
+def get_headlines(nyt_results_dict):
+    results = nyt_results_dict['results']
+    headlines = []
+    for r in results:
+        headlines.append(r['title'])
+    return headlines
+
+story_list_json = get_stories('science')
+headlines = get_headlines(story_list_json)
+
+for h in headlines:
+    print(h)
